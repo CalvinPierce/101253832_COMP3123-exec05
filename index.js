1ab8a2dcc2ac177038daa3a233ee2dcc3f893b6f
CalvinPierce <calvinpierce99@gmail.com>
2021-10-07 10:02:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const fs = require('fs')
+
+/*
+- Create new html file name home.html 
+- add <h1> tag with message "Welcome to ExpressJs Tutorial"
+- Return home.html page to client
+*/
+router.get('/home', (req,res) => {
+  //res.send('This is home router');
+  res.sendFile(__dirname + "/home.html")
+});
+
+/*
+- Return all details from user.json file to client as JSON format
+*/
+router.get('/profile', (req,res) => {
+  //res.send('This is profile router');
+  let data = fs.readFileSync("user.json")
+  let user = JSON.parse(data)
+  res.json(user)
+});
+
+/*
+- Modify /login router to accept username and password as query string parameter
+- Read data from user.json file
+- If username and  passsword is valid then send resonse as below 
+    {
+        status: true,
+        message: "User Is valid"
+    }
+- If username is invalid then send response as below 
+    {
+        status: false,
+        message: "User Name is invalid"
+    }
+- If passsword is invalid then send response as below 
+    {
+        status: false,
+        message: "Password is invalid"
+    }
+*/
+router.get('/login', (req,res) => {
+  //res.send('This is login router');
+  // localhost:8089/login?username=bret&password=bret@123 URL W/ query string parameter
+
+  let username = req.query.username
+  let password = req.query.password
+
+  let data = fs.readFileSync("user.json")
+  let user = JSON.parse(data)
+  
+  let usernameFromFile = user.username;
+  let passwordFromFile = user.password;
+
+  if (usernameFromFile === username && passwordFromFile === password) {
+    res.json({
+      status: true,
+      message: "User is valid"
+    });
+  } else if (usernameFromFile !== username) {
+    res.json({
+      status: false,
+      message: "User Name is invalid"
+    });
+  } else if (passwordFromFile !== password){
+    res.json({
+      status: false,
+      message: "Password is invalid"
+    });
+  }
+});
+
+/*
+- Modify /logout route to accept username as parameter and display message
+    in HTML format like <b>${username} successfully logout.<b>
+*/
+router.get('/logout:username', (req,res) => {
+  //res.send('This is logout router');
+  //localhost:8089/logoutCalvin
+  res.send(`<b>${req.params.username} successfully logout.<b>`);
+});
+
+app.use('/', router);
+
+app.listen(process.env.port || 8089);
+
+console.log('Web Server is listening at port '+ (process.env.port || 8089));
